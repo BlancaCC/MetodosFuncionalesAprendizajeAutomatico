7bcc42dac69c283ed77ab9711b0c7b2b6302fe83
Blanca <blancacano2.71828@gmail.com>
2023-02-16 08:36:26 +0100

    Código tras arreglar el linter (medianamente)

--- HW_01/gaussian_processes.py
+++ HW_01/gaussian_processes.py
@@ -126,23 +126,25 @@ def simulate_gp(
     #
 
     # Gaussian processes definitions
-    mean_vector = mean_fn(t) # Shape: N x 1
-    # Sources: 
+    mean_vector = mean_fn(t)  # Shape: N x 1
+
+    # Sources:
     # https://numpy.org/doc/stable/reference/generated/numpy.meshgrid.html
     # https://interactivechaos.com/es/manual/tutorial-de-numpy/la-funcion-meshgrid
-    T_1, T_2 = np.meshgrid(t, t, 
-                             indexing='ij' # matrix indexing
-                            )
-    kernel_matrix = kernel_fn(T_1, T_2)# N x N 
+    T_1, T_2 = np.meshgrid(t, t,
+                           indexing='ij'  # matrix indexing
+                           )
+    kernel_matrix = kernel_fn(T_1, T_2)  # N x N
     # We are going to carry Cholesky decomposition K(t,t) = LL^t
-    # To compute L: 
+    # To compute L:
     # Using svd decomposition: K = u s v^t
     # Since K is symmetric then u = v then K = (u sqrt(s))(u sqrt(s))^t
     # So L = u sqrt(s)
-    # Source: https://numpy.org/doc/stable/reference/generated/numpy.linalg.svd.html
+    # Source:
+    # https://numpy.org/doc/stable/reference/generated/numpy.linalg.svd.html
 
     u, s, _ = np.linalg.svd(kernel_matrix)
-    S = np.diag(s) # N x N
+    S = np.diag(s)  # N x N
     L = np.sqrt(S) @ u.T
 
     # Simulation X(t) = m(t) + LZ with Z follows a N(0, Id_n)
@@ -157,7 +159,7 @@ def build_kernel_matrix(
     t2: np.ndarray,
     kernel_fn: Callable[[np.ndarray], np.ndarray]
 ) -> np.nadarray:
-    """Build kernel matrix 
+    """Build kernel matrix
 
     Parameters
     ----------
@@ -195,14 +197,13 @@ def build_kernel_matrix(
     ...     BB_kernel,
     ... )
     >>> print(K)
-    
+
     """
 
     T_1, T_2 = np.meshgrid(t1, t2, indexing='ij')
     kernel_matrix = kernel_fn(T_1, T_2)
 
     return kernel_matrix
-
 
 
 def simulate_conditional_gp(
@@ -286,16 +287,22 @@ def simulate_conditional_gp(
     # 'svd' is slower, but numerically more robust than 'cholesky'
 
     # First we obtain the inverse matrix of kernel k(t_obs,t_obs)
-    inverse_kernel = np.linalg.inv(build_kernel_matrix(t_obs, t_obs, kernel_fn))
+    inverse_kernel = np.linalg.inv(
+        build_kernel_matrix(
+            t_obs, t_obs, kernel_fn))
 
     # Next, using the function 'build_kernel_matrix' we defined before, mean_vector and kernel_matrix are calculated using:
     # mean = m(t) + k(t, t_obs) k(t_obs, t_obs)^(-1) (X_obs - m(t_obs))
     # kernel = k(t, t) - k(t, t_obs) k(t_obs, t_obs)^(-1) k(t_obs, t)
-    mean_vector = mean_fn(t) + build_kernel_matrix(t, t_obs, kernel_fn) @ inverse_kernel @ ( x_obs - mean_fn(t_obs))
-    kernel_matrix = build_kernel_matrix(t, t, kernel_fn) - build_kernel_matrix(t, t_obs, kernel_fn) @ inverse_kernel @ build_kernel_matrix(t_obs, t, kernel_fn)
+    mean_vector = mean_fn(t) + build_kernel_matrix(t, t_obs,
+                                                   kernel_fn) @ inverse_kernel @ (x_obs - mean_fn(t_obs))
+    kernel_matrix = build_kernel_matrix(t, t, kernel_fn) - build_kernel_matrix(
+        t, t_obs, kernel_fn) @ inverse_kernel @ build_kernel_matrix(t_obs, t, kernel_fn)
 
-    # Finally the simulation, X, is obtained using 'multivariate_normal' from numpy with method 'svd'
-    X = np.random.default_rng().multivariate_normal(mean_vector, kernel_matrix, M,  method = 'svd')
+    # Finally the simulation, X, is obtained using 'multivariate_normal' from
+    # numpy with method 'svd'
+    X = np.random.default_rng().multivariate_normal(
+        mean_vector, kernel_matrix, M, method='svd')
 
     return X, mean_vector, kernel_matrix
 
@@ -357,7 +364,7 @@ def gp_regression(
     # Firstable, let's get the matrix k(X,X) + sigma^2 * I
     noise_matrix = kernel_fn(X, X) + sigma2_noise * np.identity(y.size)
 
-    # Secondly, we inverse this matrix 
+    # Secondly, we inverse this matrix
     inverse_matrix = np.linalg.solve(noise_matrix, np.identity(y.size))
 
     # Next, we store the product inverse_matrix @ y
@@ -365,9 +372,13 @@ def gp_regression(
 
     # Finally prediction_mean and prediction_variance are calculated following the equation
     # mean = k(X_test, X) (k(X, X) + sigma^2 * I)^(-1) y
-    # variance = k(X_test, X_test) - k(X_test, X) (k(X, X) + sigma^2 * I)^(-1) k(X, X_test)
-    prediction_mean = kernel_fn(X_test, X) @ inverse_matrix_y 
-    prediction_variance = kernel_fn(X_test, X_test) - kernel_fn(X_test, X) @ inverse_matrix @ kernel_fn(X, X_test)
+    # variance = k(X_test, X_test) - k(X_test, X) (k(X, X) + sigma^2 * I)^(-1)
+    # k(X, X_test)
+    prediction_mean = kernel_fn(X_test, X) @ inverse_matrix_y
+    prediction_variance = kernel_fn(X_test,
+                                    X_test) - kernel_fn(X_test,
+                                                        X) @ inverse_matrix @ kernel_fn(X,
+                                                                                        X_test)
 
     return prediction_mean, prediction_variance
 
